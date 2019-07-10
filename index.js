f823d6977cae13085d2c9059c181a99716b2a23f
Amy C <missch3n@gmail.com>
2019-07-10 22:06:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function dwarfRollCall(dwarves) {
   var dwarfNames = [];
-  for (let i=0; i < dwarves.length; i++) {
+  for (let i=0; i < dwarves.length; i++){
     dwarfNames.push(`${i+1}. ${dwarves[i]} `);
   }
   return dwarfNames.join("");
@@ -8,7 +8,7 @@ function dwarfRollCall(dwarves) {
 
 function summonCaptainPlanet(planeteerCalls) {
   var uppercase = [];
-  for (let i=0; i < planeteerCalls.length; i++) {
+  for (let i=0; i < planeteerCalls.length; i++){
     var caps = planeteerCalls[i].toUpperCase() + "!";
     uppercase.push(caps);
   }
@@ -16,7 +16,7 @@ function summonCaptainPlanet(planeteerCalls) {
 }
 
 function longPlaneteerCalls(words) {
-  for (let i=0; i < words.length; i++) {
+  for (let i=0; i < words.length; i++){
     if (words.length > 4){
       return true;
     }
@@ -28,9 +28,9 @@ function longPlaneteerCalls(words) {
 
 function findTheCheese(foods) {
   for (let i = 0; i < foods.length; i++){
-    if (foods[i].includes("cheddar")){
-      return foods[i];
+    if (foods[i] === "gouda" || foods[i] === "cheddar" || foods[i] === "camembert"){
+      return foods[i]
     }
   }
-  return "no cheese!";
+  return "no cheese!"
 }
