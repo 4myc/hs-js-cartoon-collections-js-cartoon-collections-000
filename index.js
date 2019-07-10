b5fcbb1876ac2360c54cf0d9086c467d90e7b489
Amy C <missch3n@gmail.com>
2019-07-10 17:11:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 function dwarfRollCall(dwarves) {
+  var dwarfNames = [];
+  for (let i = 0; i < dwarves.length; i++) {
+    dwarfNames.push(`${i+1}. ${dwarves[i]} `);
+  }
+  return dwarfNames.join("");
 }
 
-function summonCaptainPlanet(planeteerCalls){
+function summonCaptainPlanet(planeteerCalls) {
+  var uppercase = [];
+  for (let i=0; i < planeteerCalls.length; i++){
+    var caps = planeteerCalls[i].toUpperCase() + "!";
+    uppercase.push(caps)
+  }
+  return uppercase;
 }
 
 function longPlaneteerCalls(words) {
+  for (let i=0; i < words.length; i++){
+    if (words.length > 4){
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
 }
 
-function findTheCheese (foods) {
+function findTheCheese(foods) {
+  for (let i = 0; i < foods.length; i++){
+    if (foods[i].includes("cheddar")){
+      return foods[i]
+    }
+  }
+  return "no cheese!"
 }
