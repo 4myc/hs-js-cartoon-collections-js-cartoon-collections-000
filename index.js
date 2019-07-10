674f5eab01e5abb3a4bac6c90e79571445c1f888
Amy C <missch3n@gmail.com>
2019-07-10 18:25:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function dwarfRollCall(dwarves) {
   var dwarfNames = [];
-  for (let i = 0; i < dwarves.length; i++) {
+  for (let i=0; i < dwarves.length; i++) {
     dwarfNames.push(`${i+1}. ${dwarves[i]} `);
   }
   return dwarfNames.join("");
@@ -8,7 +8,7 @@ function dwarfRollCall(dwarves) {
 
 function summonCaptainPlanet(planeteerCalls) {
   var uppercase = [];
-  for (let i=0; i < planeteerCalls.length; i++){
+  for (let i=0; i < planeteerCalls.length; i++) {
     var caps = planeteerCalls[i].toUpperCase() + "!";
     uppercase.push(caps);
   }
@@ -16,7 +16,7 @@ function summonCaptainPlanet(planeteerCalls) {
 }
 
 function longPlaneteerCalls(words) {
-  for (let i=0; i < words.length; i++){
+  for (let i=0; i < words.length; i++) {
     if (words.length > 4){
       return true;
     }
